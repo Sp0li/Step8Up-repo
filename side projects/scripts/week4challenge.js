cfea2939afe3ca5f73f59f991e759fa0e6047e41
Oli <oliver.grove@gmail.com>
2025-02-14 20:41:41 +0000

    Final edits for week 4 challenge

--- side projects/scripts/week4challenge.js
+++ side projects/scripts/week4challenge.js
@@ -4,8 +4,21 @@ document.getElementById("addtaskButton").addEventListener("click",addNewTask)
 
 function addNewTask() {
     let TodoTask = document.getElementById("todotask").value;
-    if(TodoTask!="")
+    document.getElementById("todotask").value = "";
+    let isunique = true;
+
+    for(i=0;i<todolist.children.length;i++)
     {
+        if(todolist.children[i].innerHTML==TodoTask)
+        {
+            isunique=false;
+            document.getElementById("todotask").setAttribute("placeholder","Enter a unique task")
+        }
+    }
+
+    if(TodoTask!="" && isunique)
+    {
+        document.getElementById("todotask").setAttribute("placeholder","Enter a task")
         addItem(TodoTask);
     }
 }
@@ -22,6 +35,7 @@ function addItem(TodoTask) {
         try {
             //todolist.removeChild(todolist.children[e.target.id]);
             document.getElementById(e.target.id).remove()
+            
         } catch (TypeError) {
             console.log(todolist.children)
             for(i=0;i<todolist.children.length;i++)
@@ -33,7 +47,7 @@ function addItem(TodoTask) {
             }
         }
         
-         
+        
     })
 }
 
